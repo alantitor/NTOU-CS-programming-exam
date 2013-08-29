49e6c823a021b3bf90a41ec06d5dd81a198fe53d
Alan Titor - Debian <belosianau@gmail.com>
2013-08-29 02:31:20 -0400

    update

--- manager/GM.c
+++ manager/GM.c
@@ -1,5 +1,8 @@
 /*
- * used to manager problems
+ * used to manager problems.
+ *
+ * this program is designed to the 2013 NTOU-CS-programming-exam.
+ * if you want used it in future, please check parameters carefully.
  *
  * @Author: Alan Titor
  * @Date: 2013/8/29
@@ -9,6 +12,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include <GM.h>
+
 
 int main(int argc, char* argv[])
 {
@@ -22,34 +28,48 @@ int main(int argc, char* argv[])
 		for (int count = 1; count < argc; count++) 
 		{
 			if (argv[i] == "-h")
-				//  call help function
+				help();
 			else if (argv[i] == "-init")
-				//  call init functin
+				init();
 			else if (argv[i] == "-path")
 			{
-				if (i + 1 != argc
-					//  call set path function
+				if (i + 1 != argc)
+					set_path();
 				else
 				{
 					printf("Not enough or invalid arguments, please try again\n");
 					exit(0);
 				}
 			}
-			else if (argv[i] == "-finish")
-			//  finish coding file
-			else if (argv[i] == "-delete")
+			//  finish coding files
+			else if (argv[i] == "-finish" || argv[i] == "-f")
+			{
+				finished_file();
+			}
 			//  delete file.  change status to not yet coding.
-
-			else if (argv[i] == "-show"
+			else if (argv[i] == "-delete" || argv[i] == "-d")
+			{
+				delete_file();
+			}
+			//  show index content
 			//  show all files
 			//  show finished files
 			//  show not finished files
 			//  show not start coding files
-
+			else if (argv[i] == "-show")
+			{
+				show_index();
+			}
 			//  print index out to file
-
-			else if (argv[i] == "-eliminate"
+			else if (argv[i] == "-print")
+			{
+				print_index();
+			}
 			//  delete config and index files
+			else if (argv[i] == "-eliminate")
+			{
+				eliminate_setting();
+			}
 		}
 
 	}
